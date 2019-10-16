02800101b9a43938900d9b143b507a4a6487aa45
丁巍 <dingwei2014@xiaochuankeji.cn>
2019-10-16 09:08:00 +0800

    ADD : ① create

---
+++ Algorithm/Algorithm/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Algorithm
+//
+//  Created by 丁巍 on 2019/10/16.
+//  Copyright © 2019 丁巍. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
